9d1d7177b9a8c40ca40086249896a6b966edc67e
yoo <tjdantl10@gmail.com>
2023-12-28 10:35:52 +0900

    12/28 first

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
-from database.news_information import fetch_news_information
+from flask import Flask, render_template, request
+from database.news_information import fetch_news_information, get_news_by_genre, get_total_news_count
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # database.py에서 정의된 함수를 사용하여 뉴스 정보를 가져옴
-    news_items = fetch_news_information()
-    return render_template('it_science.html', news_items=news_items)
+    genres = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학', '연예']
+    return render_template('home.html', genres=genres)
+
+@app.route('/news/<genre>')
+@app.route('/news/<genre>/<int:page>')
+def show_news(genre, page=1):
+    genre = genre.replace('_', '/')
+    per_page = request.args.get('per_page', default=20, type=int)
+    search_query = request.args.get('search_query', '')
+    total_news_count = get_total_news_count(genre)
+    total_pages = (total_news_count + per_page - 1) // per_page
+    news_items = get_news_by_genre(genre, page, per_page, search_query)
+    genre = genre.replace('/', '_')
+    genres = ['정치', '경제', '사회', '생활/문화', '세계', 'IT/과학', '연예']
+    page_range = 10
+    start_page = max(1, ((page - 1) // page_range) * page_range + 1)
+    end_page = min(start_page + page_range - 1, total_pages)
+
+    return render_template('news.html', news_items=news_items, current_genre=genre, genres=genres, page=page, per_page=per_page, total_pages=total_pages, start_page=start_page, end_page=end_page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database/news_information.py
+++ database/news_information.py
@@ -2,11 +2,11 @@ import psycopg2
 
 def get_database_connection():
     return psycopg2.connect(
-    host="localhost",
-    dbname='final_project',
-    user='root',
-    password="5432",
-    port=5432
+    host="yourhost",
+    dbname='yousrdbname',
+    user='youruser',
+    password="yourpassword",
+    port="yourport"
 )
 
 def fetch_news_information():
@@ -19,4 +19,62 @@ def fetch_news_information():
     news_items = cursor.fetchall()
     cursor.close()
     conn.close()
+    return news_items
+
+def get_total_news_count(genre):
+    connection = get_database_connection()
+    cursor = connection.cursor()
+    query = "SELECT COUNT(*) FROM final_project.news_information WHERE news_genre = %s"
+    cursor.execute(query, (genre,))
+    count = cursor.fetchone()[0]
+    cursor.close()
+    connection.close()
+    return count
+
+
+
+# def get_news_by_genre(genre, page=1, per_page=20):
+#     offset = (page - 1) * per_page
+#     connection = get_database_connection() # 데이터베이스 연결 설정
+#     cursor = connection.cursor()
+#     query = """
+#         SELECT * FROM final_project.news_information
+#         WHERE news_genre = %s
+#         ORDER BY create_dt DESC
+#         LIMIT %s OFFSET %s
+#     """
+#     cursor.execute(query, (genre, per_page, offset))
+#     news_items = cursor.fetchall()
+#     cursor.close()
+#     connection.close()
+#     return news_items
+
+def get_news_by_genre(genre, page=1, per_page=20, search_query=''):
+    offset = (page - 1) * per_page
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    # 검색어가 있을 때만 검색 조건을 추가
+    if search_query:
+        search_terms = search_query.split()
+        search_conditions = ["news_title LIKE %s" for _ in search_terms]
+        search_query_part = " AND (" + " OR ".join(search_conditions) + ")"
+        search_patterns = [f"%{term}%" for term in search_terms]
+        query_parameters = [genre] + search_patterns + [per_page, offset]
+    else:
+        search_query_part = ""
+        query_parameters = [genre, per_page, offset]
+
+    query = f"""
+        SELECT * FROM final_project.news_information
+        WHERE news_genre = %s
+        {search_query_part}
+        ORDER BY create_dt DESC
+        LIMIT %s OFFSET %s
+    """
+
+    cursor.execute(query, query_parameters)
+    news_items = cursor.fetchall()
+    cursor.close()
+    connection.close()
     return news_items
